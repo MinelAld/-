fa3980e9a58daa2f39e5a288fde8ae7b4d806d77
MinelAld <52525137+MinelAld@users.noreply.github.com>
2019-07-09 22:59:33 +0400

    Add files via upload

---
+++ numbertwo.js
@@ -0,0 +1,18 @@
+var a=prompt ("Число a");
+var b=prompt ("Число b");
+var c=prompt ("Знак +");
+var result;
+if (c=="+")
+{
+result=(+a)+(+b);
+}else if (c=="-")
+{
+result= (+a)-(+b);
+} else if (c=="*")
+{
+result=(+a)*(+b);
+{else if (c=="/")
+{
+result=(+a)/(+b);
+}
+alert (result);
